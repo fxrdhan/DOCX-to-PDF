a07f2f5e8eb65617c7bc5c88b89be054aa6cc363
saiko_ichirou [saikou] <56776871+fxrdhan@users.noreply.github.com>
2023-03-24 15:10:25 +0700

    Edit

--- docx2pdf_single-file.py
+++ docx2pdf_single-file.py
@@ -3,19 +3,19 @@ from tkinter import filedialog
 from docx2pdf import convert
 import os
 
-# Buat interface untuk dialog box
+# Buat interface dialog box
 window = tk.Tk()
 window.withdraw()
 
 # Perintah untuk memilih file input
 input_file = filedialog.askopenfilename(title="Pilih File Input", filetypes=[("Word Document", "*.docx")])
-# Tetapkan path folder output sama dengan path folder input
+# Tetapkan path folder output yg sama dengan path folder input
 output_folder = os.path.dirname(input_file)
 
-# Tetapkan path untuk menyimpan versi PDF dari file (sama dengan path input namun dengan ekstensi PDF)
+# Tetapkan path untuk menyimpan versi PDF dari file
 pdf_path = os.path.join(output_folder, os.path.splitext(os.path.basename(input_file))[0] + ".pdf")
 
-# Konversi file menggunakan docx2pdf
+# Konversi file
 convert(input_file, pdf_path)
 
 # Tampilkan pesan berhasil
